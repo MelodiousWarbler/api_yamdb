4c0de26d137699f184be9892cff41a9d2990596a
Артем Никишин <artni96@yandex.ru>
2022-12-20 13:13:04 +0300

    Написаны модель Category, Group и Title. Добавлены в админку

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, Title, Category, Genre
 
 
 @admin.register(User)
@@ -17,3 +17,44 @@ class UserAdmin(admin.ModelAdmin):
     search_fields = ('username', 'role',)
     list_filter = ('username',)
     empty_value_display = '-пусто-'
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    fields = (
+        'title',
+        'category',
+        'genre',
+    )
+    list_display = (
+        'title',
+        'category',
+    )
+    search_fields = (
+        'title',
+        'category',
+        'genre',
+    )
+    list_filter = (
+        'category',
+        'genre',
+    )
+    filter_horizontal = (
+        'genre',
+    )
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'description',
+    )
+    search_fields = (
+        'name',
+    )
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Category)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -76,3 +76,47 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=128)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Title(models.Model):
+    title = models.CharField(max_length=128)
+    category = models.ForeignKey(
+        Category,
+        related_name='titles',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    genre = models.ManyToManyField(Genre)
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.title
