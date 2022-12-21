681dde5ca0d8bd99793f8f5fd779eaa0cb8b065e
MelodiousWarbler <63454948+MelodiousWarbler@users.noreply.github.com>
2022-12-21 17:47:54 +0300

    Самоуправство с пермишенами

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,13 +1,12 @@
 from rest_framework import permissions
 
-from reviews.models import ADMIN
-
 
 class AdminOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
             request.user.is_admin
             or request.user.is_staff
+            or request.user.is_superuser
         )
 
     def has_object_permission(self, request, view, obj):
@@ -18,9 +17,10 @@ class AdminOnly(permissions.BasePermission):
 
 
 class isAdminOrReadOnly(permissions.BasePermission):
-
-    def has_object_permission(self, request, view, obj):
+    def has_permission(self, request, view):
         return (
-            request.user.role is ADMIN
+            request.user.is_admin
+            or request.user.is_staff
             or request.method in permissions.SAFE_METHODS
+            or request.user.is_superuser
         )
